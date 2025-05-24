0b94cebf7a5377b5410aea60517b07d748c7f703
Vladimir Lekhterev <vovalekhterev@gmail.com>
2025-05-24 18:55:50 +0300

    Test all procedures

--- Src/Test.cpp
+++ Src/Test.cpp
@@ -1,6 +1,6 @@
 #include <ZubrRPC.hpp>
-#include <iostream>
 #include <cassert>
+#include <iostream>
 
 using ZP = Zubr::RPC::Procedures;
 
@@ -19,41 +19,41 @@ int main(int argc, char** argv) {
   conf.Port = "/dev/ttyACM0";
 
   auto io = Zubr::IO(conf);
-  Zubr::RPC::Call<ZP::GetZubrParam>(io, {0});
-  Zubr::RPC::Call<ZP::GetZubrParam>(io, {0});
-  Zubr::RPC::Call<ZP::GetZubrParam>(io, {0});
 
+  Zubr::RPC::Call<ZP::SetZubrParam>(io);
+  Zubr::RPC::Call<ZP::GetZubrParam>(io);
+  Zubr::RPC::Call<ZP::UnitParam>(io);
+  Zubr::RPC::Call<ZP::SetPosition>(io);
+  Zubr::RPC::Call<ZP::GetPosition>(io);
+  Zubr::RPC::Call<ZP::GetVelocity>(io);
+  Zubr::RPC::Call<ZP::GetIMU>(io);
+  Zubr::RPC::Call<ZP::StartSlot>(io);
+
+  /*
   Zubr::CsMessageOut msgOut;
-  msgOut.hostBeginQuery(RB_CB_RL_STATE_GET);
+  msgOut.hostBeginQuery(RB_CB_RL_IMU_GET);
+  msgOut.hostEnd();
   
-  //msgOut.addInt8(14);
-  
-  //for (int i = 0; i < 25; ++i)
-  //  msgOut.addInt16(32767);
-  
-  //msgOut.hostEnd();
   io.Send(msgOut);
-  
-  
+  */
   try {
-    auto msgIn = io.Recv(nRecv);
-    
-    //auto resp = Zubr::RPC::Call<ZP::GetZubrParam>(io, {0});
-    
-    //std::cout << "Sio: " << +resp.Sio() << std::endl;
-    //std::cout << "Oper: " << +resp.Oper() << std::endl;
-    //std::cout << "Id: " << +resp.Id() << std::endl;
-    //std::cout << "Index: " << +resp.Index() << std::endl;
-    //std::cout << "Value: " << +resp.Value() << std::endl;
+    //auto msgIn = io.Recv(nRecv);
 
-  } catch (std::exception &e) {
+    //auto resp = Zubr::RPC::Call<ZP::SetZubrParam>(io);
+
+    // std::cout << "Sio: " << +resp.Sio() << std::endl;
+    // std::cout << "Oper: " << +resp.Oper() << std::endl;
+    // std::cout << "Id: " << +resp.Id() << std::endl;
+    // std::cout << "Index: " << +resp.Index() << std::endl;
+    // std::cout << "Value: " << +resp.Value() << std::endl;
+
+  } catch (std::exception& e) {
     std::cout << "Got exception: " << e.what() << std::endl;
   }
 
   std::cout << std::hex;
-  for (int i = 0; i < nRecv; ++i)
-    std::cout << +io.RxBuf[i] << " ";
+  for (int i = 0; i < nRecv; ++i) std::cout << +io.RxBuf[i] << " ";
   std::cout << std::dec << std::endl;
-  
+
   return 0;
 }
