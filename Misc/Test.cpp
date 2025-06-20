a646788425cd6b5747cca60140c8f893965692f1
Vladimir Lekhterev <vovalekhterev@gmail.com>
2025-06-20 20:12:02 +0300

    Update test once again

--- Misc/Test.cpp
+++ Misc/Test.cpp
@@ -8,41 +8,17 @@ using namespace Zubr::Printers;
 int main(int argc, char** argv) try {
   Zubr::Zubr zubr ("/dev/ttyACM0");
 
-  auto imu = zubr.GetIMU();
-  auto position = zubr.GetPosition();
-  auto velocity = zubr.GetVelocity();
+  auto pos = zubr.SetPosition({});
+  std::cout << pos << std::endl;
+  
+  pos = zubr.GetPosition();
+  std::cout << pos << std::endl;
 
-  double x = 0;
+  auto vel = zubr.GetVelocity();
+  std::cout << vel << std::endl;
 
-  while(x < 15) {
-    position.Frame++;
-    position.Values[4] = fabs(sin(x));
-    x+= .01;
-    zubr.SetPosition(position);
-    
-    auto newPos = zubr.GetPosition();
-    auto vel = zubr.GetVelocity();
-
-    for (int i = 0; i < position.Values.size(); ++i) {
-      std::cout << position.Values[i] << " ";
-    }
-    std::cout << std::endl;
-
-    for (int i = 0; i < newPos.Values.size(); ++i) {
-      std::cout << newPos.Values[i] << " ";
-    }
-    std::cout << std::endl;
-
-    for (int i = 0; i < vel.Values.size(); ++i) {
-      std::cout << vel.Values[i] << " ";
-    }
-    std::cout << std::endl;
-
-
-    //std::cout << zubr.GetPosition() << std::endl;
-    //std::cout << zubr.GetVelocity() << std::endl;
-    usleep(1000*10);
-  }
+  auto IMU = zubr.GetIMU();
+  std::cout << IMU << std::endl;
 
   return 0;
 } catch (std::exception& e) {
